e213a8399c4b8d4c5ac0a103ebbbc75ad30d9f17
Moutia Benachour <motie.benachour@gmail.com>
2022-01-28 10:10:17 +0100

    update name registery form

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -1,69 +1,149 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {newContextComponents} from "@drizzle/react-components";
 import {NameEntryForm} from "./NameEntryForm";
 
 const {AccountData, ContractData} = newContextComponents;
 
 export default ({drizzle, drizzleState}) => {
+    const [blockNumber, setBlockNumber] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(async () => {
+            setBlockNumber(await drizzle.web3.eth.getBlockNumber());
+        }, 5000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, []);
+    window.appDrizzle = drizzle;
+    const [nameEntryConfig, setNameEntryConfig] = useState(null);
+
     // destructure drizzle and drizzleState from props
     return (
         <div className="App">
-            <div className="row">
-                <div className="col">
-                    <div className="title is-4">
-                        Register new name
-                    </div>
-
-                    <NameEntryForm
-                        drizzle={drizzle}
-                        drizzleState={drizzleState}
-                        initialText={''}
-                    />
-
-                    <div className="title is-4">
-                        Registered Names
-                    </div>
-
-                    return <div>
-                    <ContractData
-                        drizzle={drizzle}
-                        drizzleState={drizzleState}
-                        contract='NameRegistery'
-                        method="fetchPage"
-                        methodArgs={[drizzle.accounts[0]]}
-                        render={function (items) {
-                            if (!items) {
-                                return <div>Could not load names..</div>
-                            }
-                            return <div>
-                                {
-                                    items.map(address => <div className="card mb-5" key={address}>
-                                        <div className="card-content">
-                                            <NameEntryForm
-                                                drizzle={drizzle}
-                                                drizzleState={drizzleState}
-                                                initialText={''}
-                                            />
-                                        </div>
-                                    </div>)
-                                }
-                            </div>
-                        }}
-                    />
+            <div className={`modal ${nameEntryConfig ? 'is-active' : ''}`}>
+                <div className="modal-background"></div>
+                <div className="modal-content">
+                    {nameEntryConfig &&
+                        <>
+                            <NameEntryForm
+                                drizzle={drizzle}
+                                drizzleState={drizzleState}
+                                initialText={nameEntryConfig.initialText}
+                                method={nameEntryConfig.method}
+                            />
+                        </>
+                    }
                 </div>
+                <button className="modal-close is-large" aria-label="close"
+                        onClick={() => setNameEntryConfig(null)}
+                ></button>
+            </div>
 
-                    <div className="col is-4-tablet is-3-desktop">
-                        <h2>Active Account</h2>
-                        <AccountData
-                            drizzle={drizzle}
-                            drizzleState={drizzleState}
-                            accountIndex={0}
-                            units="ether"
-                            precision={3}
-                        />
+            <div className="section">
+                <div className="container">
+                    <div className="columns">
+                        <div className="column">
+                            <div className="level is-mobile">
+                                <div className="level-left">
+                                    <div className="title is-4">
+                                        Registered Names
+                                    </div>
+                                </div>
+                                <div className="level-right">
+                                    <button className="button is-primary is-outlined" onClick={() => {
+                                        setNameEntryConfig({
+                                            initialText: '',
+                                            method: 'register',
+                                        })
+                                    }}>
+                                        Register new
+                                    </button>
+                                </div>
+                            </div>
+
+
+                            <div>
+                                <ContractData
+                                    drizzle={drizzle}
+                                    drizzleState={drizzleState}
+                                    contract='NameRegistry'
+                                    method="fetchUserRegisteredNames"
+                                    render={function (items) {
+                                        const account = drizzleState.accounts[0];
+                                        if (!items) {
+                                            return <div>No names registered..</div>
+                                        }
+
+                                        return <div>
+                                            {
+                                                items.filter(item => item.owner === account)
+                                                    .map(item => {
+                                                        const name = drizzle.web3.utils.hexToAscii(item.name).replaceAll('\x00', '');
+                                                        return <div className="card mb-5 renew-card" key={item}>
+                                                            <div className="card-content">
+                                                                <div className="level is-mobile">
+                                                                    <div className="level-left">
+                                                                        <div>
+                                                                            <div className="has-text-weight-bold has-big-font">
+                                                                                {name}
+                                                                            </div>
+                                                                            {/*<div className="has-text-grey has-small-font">*/}
+                                                                            {/*    {item.expires_at}*/}
+                                                                            {/*</div>*/}
+                                                                        </div>
+
+                                                                    </div>
+
+                                                                    <div className="level-right">
+                                                                        {item.expires_at > blockNumber
+                                                                            ? <>
+                                                                                <div className="tag is-info mr-2"
+                                                                                     onClick={() => {
+                                                                                         setNameEntryConfig({
+                                                                                             method: 'renew',
+                                                                                             initialText: name,
+                                                                                         })
+                                                                                     }}
+                                                                                >renew
+                                                                                </div>
+                                                                                <div className="tag is-danger">cancel
+                                                                                </div>
+                                                                            </>
+                                                                            :
+                                                                            <div className="tag">expired</div>
+                                                                        }
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>;
+                                                    })
+                                            }
+                                        </div>
+                                    }}
+                                />
+                            </div>
+
+                        </div>
+                        <div className="column is-4-tablet is-3-desktop">
+                            <div className="card">
+                                <div className="card-content" style={{overflow: 'hidden'}}>
+                                    <div className="title">Active Account</div>
+                                    <AccountData
+                                        drizzle={drizzle}
+                                        drizzleState={drizzleState}
+                                        accountIndex={0}
+                                        units="ether"
+                                        precision={3}
+                                    />
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
--- app/src/NameEntryForm.js
+++ app/src/NameEntryForm.js
@@ -1,93 +1,148 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {newContextComponents} from "@drizzle/react-components";
 
 const {ContractForm} = newContextComponents;
 
 const BLOCK_RESERVATION_COST = 1;
 
+function capitalize(str) {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.toLowerCase().slice(1)).join(' ');
+}
+
 export function NameEntryForm({
                                   drizzleState,
                                   drizzle,
                                   initialText,
+                                  method
                               }) {
     const [text, setText] = useState((initialText || '').trim());
-    const [investmentAmount, setInvestmentAmount] = useState();
+    const [numberOfBlocks, setNumberOfBlocks] = useState('0');
+    const [transactionStatus, setTransactionStatus] = useState('');
 
     return <>
         <div className="card mt-4">
             <div className="card-content">
+                <div className="title is-4">
+                    {capitalize(method)} name
+                </div>
+
                 <ContractForm
                     drizzle={drizzle}
                     drizzleState={drizzleState}
-                    contract="NameRegistry"
-                    method="invest"
-                    sendArgs={{value: investmentAmount, from: drizzleState.accounts[0], gaz: 5.4 * 1000 * 1000}}
+                    contract='NameRegistry'
+                    method={method}
+                    sendArgs={{value: numberOfBlocks * BLOCK_RESERVATION_COST, gas: 1000 * 1000}}
                     render={({inputs, inputTypes, state, handleInputChange, handleSubmit}) => {
                         const max = 100;
-                        if (max === 0) {
-                            return <div className="message">
-                                <div className="message-body">No enough cash. Stranger!</div>
-                            </div>
-                        }
 
                         return (
-                            <form onSubmit={handleSubmit}>
-                                <div className="field">
-                                    <label className="label" htmlFor="investmentAmount">
-                                        Name
-                                    </label>
+                            <form onSubmit={(e) => {
+                                e.persist();
+                                e.preventDefault();
 
-                                    <div className="control">
-                                        <input
-                                            style={{maxWidth: '280px'}}
-                                            className="input"
-                                            name="name"
-                                            required
-                                            type="text"
-                                            value={text}
-                                            onChange={(event) => {
-                                                setText(
-                                                    event.target.value
-                                                );
-                                                handleInputChange(event);
-                                            }}
-                                        />
+                                if (numberOfBlocks > max) {
+                                    return;
+                                }
+
+                                handleInputChange({
+                                    target: {
+                                        name: 'name',
+                                        type: 'text',
+                                        value: text
+                                    }
+                                });
+
+                                handleInputChange({
+                                    target: {
+                                        name: 'expires_after',
+                                        type: 'number',
+                                        value: parseInt(numberOfBlocks)
+                                    }
+                                });
+
+                                const listenToTransactionStatus = (transactionStackIdx) => {
+                                    // TODO: use subscription from store instead
+                                    const interval = setInterval(() => {
+                                        const item = drizzleState.transactions[
+                                            drizzleState.transactionStack[transactionStackIdx]
+                                        ];
+
+                                        setTransactionStatus(item.status);
+                                    }, 3000);
+                                }
+
+                                setTimeout(() => {
+                                    const transactionStackIdx = handleSubmit(e);
+
+                                    listenToTransactionStatus(transactionStackIdx);
+                                });
+                            }}>
+                                <div className="columns">
+                                    <div className="column is-8">
+                                        <div className="field">
+                                            <label className="label" htmlFor="numberOfBlocks">
+                                                Name
+                                            </label>
+
+                                            <div className="control">
+                                                <input
+                                                    className="input"
+                                                    name="name"
+                                                    required
+                                                    type="text"
+                                                    value={text}
+                                                    readOnly={!!initialText}
+                                                    onChange={(event) => {
+                                                        if (initialText) return;
+
+                                                        setText(
+                                                            event.target.value
+                                                        );
+                                                    }}
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="column is-4">
+                                        <div className="field">
+                                            <label className="label" htmlFor="numberOfBlocks">
+                                                Number of blocks
+                                            </label>
+
+                                            <div className="control">
+                                                <input
+                                                    style={{maxWidth: '280px'}}
+                                                    name="expires_after"
+                                                    className="input"
+                                                    required
+                                                    type="number"
+                                                    step="1"
+                                                    min="1"
+                                                    max={max}
+                                                    value={numberOfBlocks}
+                                                    onChange={(event) => {
+                                                        setNumberOfBlocks(
+                                                            `${event.target.value
+                                                                ? parseInt(event.target.value) * BLOCK_RESERVATION_COST
+                                                                : 0}`
+                                                        );
+                                                    }}
+                                                />
+                                            </div>
+                                        </div>
+
                                     </div>
                                 </div>
 
-                                <div className="field">
-                                    <label className="label" htmlFor="investmentAmount">
-                                        Number of blocks
-                                    </label>
 
-                                    <div className="control">
-                                        <input
-                                            style={{maxWidth: '280px'}}
-                                            name="expires_after"
-                                            className="input"
-                                            required
-                                            type="number"
-                                            step="1"
-                                            min="1"
-                                            max={max}
-                                            onChange={(event) => {
-                                                setInvestmentAmount(
-                                                    event.target.value
-                                                        ? parseInt(event.target.value) * BLOCK_RESERVATION_COST
-                                                        : 0
-                                                );
-                                                handleInputChange(event);
-                                            }}
-                                        />
-                                    </div>
-                                </div>
-
-                                <button type="submit" className="button is-primary">Submit</button>
+                                <button type="submit" className="button is-primary"
+                                    disabled={numberOfBlocks > max}
+                                >Submit</button>
                             </form>
                         );
                     }}
                 />
-                }
             </div>
         </div>
     </>;
